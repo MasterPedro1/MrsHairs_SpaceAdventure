a3ab79fc5e4b1e5db3a2475930466a06522de208
Alam Armas <carlosalam33@hotmail.com>
2023-05-17 13:57:33 -0600

    Singleton para GM y método para puntuación

--- MrsHairs_SpaceAdventure/Assets/Scripts/GameManager.cs
+++ MrsHairs_SpaceAdventure/Assets/Scripts/GameManager.cs
@@ -1,8 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    [SerializeField] float coins;
+    public float Coins { get { return coins; } set { coins = value; } }
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+    public void IncreaseScore(TextMeshProUGUI text, float increaseValue)
+    {
+        coins += increaseValue;
+        text.text = coins.ToString();
+    }
+
+
+    public void DecreaseScore(TextMeshProUGUI text, float increaseValue)
+    {
+        coins -= increaseValue;
+        if (coins <= 0) 
+        {
+            coins = 0;
+            text.text = coins.ToString();
+            return; 
+        }
+        text.text = coins.ToString();
+    }
 }
